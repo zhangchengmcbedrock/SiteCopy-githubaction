f853c8f48744173f3a212f0a636e4f27dd711123
zhangchengmcbedrock <120537551+zhangchengmcbedrock@users.noreply.github.com>
2026-01-27 23:01:43 +0800

    Refactor sitecopy: restructure, add workflow and batch script
    Moved workflow YAML files to an 'old' subdirectory and added a new web-sitecopy workflow. Refactored the sitecopy Python script and requirements into a new directory structure under 'sitecopy bash/python'. Added a Windows batch script for automated sitecopy operations. Improved coroutine handling in sitecopy.py for better event loop management.

---
+++ sitecopy bash/sitecopy-web.bat
@@ -0,0 +1,25 @@
+echo install dependencies
+pip install bs4 Path argparse requests
+echo run python script
+echo start sitecopy
+echo main website
+python ./python/sitecopy.py -u "https://19870310.xyz/" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://qbxd.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://gmxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://jczx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://kcxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://mh3z.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://mh4z.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://mjyfz.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://mqsx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://pj1z.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://qbxd.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://qzxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://sszx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://stx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://wjzx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://xcxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://xgxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+python ./python/sitecopy.py -u "https://zdxx.19870310.xyz/sitecopy" -e -d 1000 -t 50
+echo delete website folder
+rd /s /q "website"
